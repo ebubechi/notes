174ce44d545b49f7e79d007f67c3e811d926285c
ebubechi <emmanuelsonzico@gmail.com>
2023-07-21 23:23:35 +0100

    error handling with bloc

--- lib/views/auth/login_view.dart
+++ lib/views/auth/login_view.dart
@@ -8,6 +8,7 @@ import 'package:notes/services/auth/auth_exceptions.dart';
 import 'package:notes/services/auth/bloc/auth_bloc.dart';
 import 'package:notes/services/auth/bloc/auth_event.dart';
 
+import '../../services/auth/bloc/auth_state.dart';
 import '../../utilities/dialogs/show_error_dialog.dart';
 
 class LoginView extends StatefulWidget {
@@ -64,22 +65,43 @@ class _LoginViewState extends State<LoginView> {
                   const InputDecoration(hintText: 'Enter your password here'),
               obscureText: true,
             ),
-            TextButton(
-              child: const Text('Login'),
-              onPressed: () async {
-                final email = _email!.text;
-                final password = _password!.text;
-                try {
-                  context.read<AuthBloc>().add(AuthEventLogIn(email, password));
-               // await AuthService.firebase().logIn(email: email, password: password);
-                } on UserNotFoundAuthException {
-                  await showErrorDialog(context, 'User not found');
-                } on WrongPasswordAuthException {
-                  await showErrorDialog(context, 'Wrong password');
-                } on GenericAuthException {
-                  await showErrorDialog(context, 'Authentication error');
+            BlocListener<AuthBloc, AuthState>(
+              listener: (context, state) async {
+                if (state is AuthStateLoggedOut) {
+                  if (state.exception is UserNotFoundAuthException) {
+                    await showErrorDialog(context, 'User not found');
+                  } else if (state.exception is WrongPasswordAuthException) {
+                    await showErrorDialog(context, 'Wrong credentials');
+                  } else {
+                    await showErrorDialog(context, 'Authentication Error');
+                  }
                 }
               },
+              child: TextButton(
+                child: const Text('Login'),
+                onPressed: () async {
+                  final email = _email!.text;
+                  final password = _password!.text;
+                  // try {
+                  //   context
+                  //       .read<AuthBloc>()
+                  //       .add(AuthEventLogIn(email, password));
+                  //   // await AuthService.firebase().logIn(email: email, password: password);
+                  // } on UserNotFoundAuthException {
+                  //   await showErrorDialog(context, 'User not found');
+                  // } on WrongPasswordAuthException {
+                  //   await showErrorDialog(context, 'Wrong password');
+                  // } on GenericAuthException {
+                  //   await showErrorDialog(context, 'Authentication error');
+                  // }
+                  context.read<AuthBloc>().add(
+                        AuthEventLogIn(
+                          email,
+                          password,
+                        ),
+                      );
+                },
+              ),
             ),
             TextButton(
                 onPressed: () {
